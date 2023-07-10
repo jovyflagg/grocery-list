39c32e62b66a8f099852c0404db3a99b4062e862
Jovy Cortez <jovycortez11@gmail.com>
2023-07-10 15:28:36 -0400

    clearList and removeItem functions added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     else if(name && isEditing){
 // deal with it
     } else {
-      //show alert
+      showAlert(true, 'success', 'item added to the list')
       const newItem = {id: new Date().getTime().toString(), title: name}
       setList([...list,newItem]);
       setName('');
@@ -24,6 +24,16 @@ function App() {
   }
   const showAlert = (show=false, type="", msg="") => {
     setAlert({show, type, msg})
+  }
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id))
   }
 
   return (
@@ -45,8 +55,8 @@ function App() {
       </form>
       {list.length > 0 && (
       <div className="grocery-container">
-        <List items={list} />
-        <button className="clear-btn">clear items</button>
+        <List items={list} removeItem={removeItem}/>
+        <button className="clear-btn" onClick={clearList}>clear items</button>
       </div>
       )}
     </section>
--- src/List.js
+++ src/List.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
+const List = ({ items, removeItem }) => {
   return (
     <div className="grocery-list">
-      {items.map((item)=>{
-        const {id, title} = item;
+      {items.map((item) => {
+        const { id, title } = item;
         return <article key={id} className="grocery-item">
           <p className="title">{title}</p>
           <div className="btn-container">
             <button type="button" className='edit-btn'>
               <FaEdit />
             </button>
-            <button type="button" className='delete-btn'>
+            <button type="button"
+              className='delete-btn'
+              onClick={() => removeItem(id)}
+            >
               <FaTrash />
             </button>
           </div>
